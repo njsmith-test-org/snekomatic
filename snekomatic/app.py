339634e38cee39a5ed45618a789854e98e865ee9
Nathaniel J. Smith <njs@pobox.com>
2020-02-23 16:38:27 -0800

    Prime database on startup

--- snekomatic/app.py
+++ snekomatic/app.py
@@ -10,7 +10,7 @@ from quart_trio import QuartTrio
 import gidgethub
 from gidgethub.sansio import accept_format
 
-from .db import SentInvitation
+from .db import SentInvitation, get_conn
 from .gh import GithubApp, reply_url, reaction_url
 
 # we should stash the delivery id in a contextvar and include it in logging
@@ -214,6 +214,10 @@ async def handle_test_job(command, event_type, payload, gh_client):
 
 async def main(*, task_status=trio.TASK_STATUS_IGNORED):
     print("~~~ Starting up! ~~~")
+    # Make sure database connection works, schema is up to date, run any
+    # required migrations, etc.
+    with get_conn() as conn:
+        pass
     # On Heroku, have to bind to whatever $PORT says:
     # https://devcenter.heroku.com/articles/dynos#local-environment-variables
     port = os.environ.get("PORT", 8000)
