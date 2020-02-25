f3d34d4cefa9ae3e93ff4e05ce5cc05ecd53f56d
Nathaniel J. Smith <njs@pobox.com>
2020-02-25 06:08:41 -0800

    First pass a db updates for worker jobs

--- snekomatic/db.py
+++ snekomatic/db.py
@@ -1,7 +1,14 @@
 import os
 from pathlib import Path
-from sqlalchemy import create_engine, MetaData, Table, Column, String
-from sqlalchemy.sql.expression import select, exists
+from sqlalchemy import create_engine, MetaData, Table, Column, ForeignKey
+from sqlalchemy.types import (
+    String,
+    Integer,
+    Boolean,
+    DateTime,
+    JSON,
+)
+from sqlalchemy.sql.expression import select, exists, text
 import alembic.config
 import alembic.command
 import alembic.migration
@@ -9,12 +16,41 @@ import alembic.autogenerate
 import pprint
 import attr
 
-metadata = MetaData()
+# Required to make sure that constraints like ForeignKey get a stable name so
+# migration can be supported.
+naming_convention = {
+    "ix": "ix_%(column_0_label)s",
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s",
+}
+
+metadata = MetaData(naming_convention=naming_convention)
 
 sent_invitation = Table(
     "persistent_set_sent_invitation",
     metadata,
     Column("entry", String, primary_key=True),
+)
+
+worker_jobs = Table(
+    "worker_jobs",
+    metadata,
+    Column("job_id", String, primary_key=True),
+    Column("args", JSON, nullable=False),
+    Column("started", DateTime, nullable=False),
+    Column("finished", Boolean, nullable=False),
+)
+
+worker_job_events = Table(
+    "worker_job_messages",
+    metadata,
+    Column("message_id", Integer, primary_key=True),
+    Column(
+        "job_id", String, ForeignKey("worker_jobs.job_id"), nullable=False
+    ),
+    Column("message", JSON, nullable=False),
 )
 
 
@@ -32,13 +68,30 @@ def get_conn():
     if CACHED_ENGINE.database_url != os.environ["DATABASE_URL"]:
         engine = create_engine(os.environ["DATABASE_URL"])
 
-        # Run any necessary migrations
-        with engine.connect() as conn:
-            alembic_cfg = alembic.config.Config(
-                Path(__file__).parent / "alembic.ini"
-            )
-            alembic_cfg.attributes["connection"] = conn
-            alembic.command.upgrade(alembic_cfg, "head")
+        # Set this *temporarily* in a *test* environment to reset the database
+        # at startup. Useful if you're iterating on db schema changes and
+        # aren't ready to mess with alembic migrations yet.
+        if "DESTRUCTIVE_TESTING_RESET_DB" in os.environ:
+            assert "test" in os.environ["HEROKU_APP_NAME"]
+            print("-- DESTRUCTIVE TESTING ENABLED; WIPING DB --")
+            with engine.connect() as conn:
+                # https://stackoverflow.com/questions/3327312/how-can-i-drop-all-the-tables-in-a-postgresql-database
+                conn.execute(text("""
+                  DROP SCHEMA public CASCADE;
+                  CREATE SCHEMA public;
+                  GRANT ALL ON SCHEMA public TO postgres;
+                  GRANT ALL ON SCHEMA public TO public;
+                  COMMIT;
+                """))
+            metadata.create_all(engine)
+        else:
+            # Run any necessary migrations
+            with engine.connect() as conn:
+                alembic_cfg = alembic.config.Config(
+                    Path(__file__).parent / "alembic.ini"
+                )
+                alembic_cfg.attributes["connection"] = conn
+                alembic.command.upgrade(alembic_cfg, "head")
 
         # Verify that the actual final schema matches what we expect
         with engine.connect() as conn:
